89f82b9d914b39d2d5414ff6d4c377ddaf51ef4e
Patrick Geertz <geertz.patrick@web.de>
2024-10-10 22:10:42 +0200

    renamed cli project

---
+++ CsvAccess.CLI/Program.cs
@@ -0,0 +1,15 @@
+﻿using csvAccess.core.Models.Data.Columns;
+using PostgreSqlWrapper;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestingSpace.Demo();
+        }
+    }
+}
