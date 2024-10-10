2d690c8e3c88d90144c950019018ea4355cd2801
Patrick Geertz <geertz.patrick@web.de>
2024-10-10 22:08:37 +0200

    extended Core for Database interfaces

---
+++ csvAccess.core/Repositories/ITableRepository.cs
@@ -0,0 +1,15 @@
+﻿using csvAccess.core.Models.Data.Table;
+using CsvAccess.core.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgreSqlWrapper.Table.Get
+{
+    public interface ITableRepository
+    {
+        public DataTable GetTable(ICredentials credentials, string tableName);
+    }
+}
