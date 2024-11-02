7edecce627d0efb8cbc9a5c291b472d6eabdaf31
Patrick Geertz <geertz.patrick@web.de>
2024-11-02 14:30:20 +0100

    cleanup

--- csvAccess.core/Models/Persistence/ConnectionService.cs
+++ csvAccess.core/Models/Persistence/ConnectionService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CsvAccess.core.Models.Persistence
+﻿namespace CsvAccess.core.Models.Persistence
 {
     public interface ConnectionService
     {
